ba384c01bbb5d2888d65b1240d39fb402e83e353
3kutl <3kutl@EMREKUTLU>
2023-07-23 22:48:41 +0300

    Add project files.

---
+++ CarReviewAPI/Models/Director.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieReviewAPI.Models
+{
+    public class Director
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<MovieDirector> MovieDirectors { get; set; }
+    }
+}
